df330341e54c72bb300b93d01960d6e400e3e37b
Pratyush Tiwari <pratyush3217@gmail.com>
2025-11-17 13:00:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function goToSignup() {
+  window.location.href = "signup.html"; // will create this page next
+}
+
+function goToLogin() {
+  window.location.href = "login.html"; // will create this page next
+}
