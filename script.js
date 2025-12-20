0c9c5d9d726d62df1f27699ae2fe7adb94023bd5
Pratyush Tiwari <pratyush3217@gmail.com>
2025-12-20 10:01:24 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function goToSignup() {
-  window.location.href = "signup.html"; // will create this page next
-}
-
-function goToLogin() {
-  window.location.href = "login.html"; // will create this page next
-}
